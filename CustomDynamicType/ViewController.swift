41f8192358781c2d89c5e7962f26c259802eb25f
Daniel Hu <hdmdhr@gmail.com>
2022-07-10 13:44:58 -0600

    Initial Commit

---
+++ CustomDynamicType/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomDynamicType
+//
+//  Created by 胡洞明 on 2022-07-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
