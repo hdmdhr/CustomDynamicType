68dfddd791cb382f2e493779e15353bd4ed6fb10
Daniel Hu <hdmdhr@gmail.com>
2022-07-10 19:08:23 -0600

    Add @IBOutlets

--- CustomDynamicType/ViewController.swift
+++ CustomDynamicType/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var largeTitleLabel: UILabel!
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var subheadLabel: UILabel!
+    @IBOutlet weak var titleLabe: UILabel!
+    @IBOutlet weak var calloutLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var footnoteLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
