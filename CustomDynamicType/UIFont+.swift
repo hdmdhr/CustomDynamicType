555fbaefeed521511c8031bab058c98e8f78dfd5
Daniel Hu <hdmdhr@gmail.com>
2022-07-10 23:15:06 -0600

    Add UIFont extension method.

--- CustomDynamicType/UIFont+.swift
+++ CustomDynamicType/UIFont+.swift
@@ -9,6 +9,16 @@ import UIKit
 
 extension UIFont {
     
-    
+    class func preferredFont(forTextStyle style: UIFont.TextStyle, weight: Weight, size: CGFloat? = nil) -> UIFont {
+        // 1
+        let metrics = UIFontMetrics(forTextStyle: style)
+        // 2
+        let descriptor = preferredFont(forTextStyle: style).fontDescriptor
+        let defaultSize = descriptor.pointSize
+        // 3
+        let fontToScale = UIFont.systemFont(ofSize: size ?? defaultSize, weight: weight)
+        // 4
+        return metrics.scaledFont(for: fontToScale)
+    }
     
 }
